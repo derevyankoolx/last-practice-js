2ff4be552d97717c765fc5c7d5ed34581f6f8121
Olexander Derevianko <derevyankoolx@gmail.com>
2022-06-27 10:49:17 +0300

    practiceJS

--- js/script.js
+++ js/script.js
@@ -192,20 +192,104 @@
 
 // lesson 10
 
-a = 5;
+// a = 5;
 
-console.log(a);
+// console.log(a);
 
-let wreo;
+// let wreo;
 
-console.log(wreo);
+// console.log(wreo);
+
+
 
 
 // lesson 11
 
 
+const numberOfFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = ('На сколько оцените его',''),
+      c = ('Один из последних просмотренных фильмов', ''),
+      d = ('На сколько оцените его','');
+
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+if (1) {
+    console.log('Ok!');
+}  else {
+    console.log('Error');
+}
+
+const num = 50;
+if(num < 49) {
+    console.log('Error');
+}   else if (num > 100){
+    console.log('Много');
+} else {
+    console.log('Ok');
+}
+
+(num == 50) ? console.log("Ok") : console.log('Error');
+
+
+
 // lesson 12
 
+
+if (1) {
+    console.log('Ok!');
+}   else {
+    console.log('Error');
+}
+
+
+// if(number < 49) {
+//     console.log('Error');
+// } else if (number > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (number === 50) ? console.log('Ok!') : console.log('Error');
+
+const number = 50;
+
+switch(number) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
 
 // lesson 13
 
